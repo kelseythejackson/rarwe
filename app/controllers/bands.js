aa88d77895364558b2bd8e632a41c69fec94b800
Kelsey Jackson <kelseythej@gmail.com>
2020-01-07 13:44:25 -0600

    Controllers
    wires up a controller for actions, Extracting Model Classes on page 79 is next

---
+++ app/controllers/bands.js
@@ -0,0 +1,20 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+export default Controller.extend({
+  isAddingBand: false,
+  newBandName: '',
+
+  addBand: action(function() {
+    this.set('isAddingBand', true)
+  }),
+
+  cancelAddBand: action(function() {
+    this.set('isAddingBand', false);
+  }),
+
+  saveBand: action(function() {
+    
+  })
+
+});
