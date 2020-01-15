6a81e3a7eae5b0f455bf9695367eacb39f335112
Kelsey Jackson <kelseythej@gmail.com>
2020-01-15 11:54:24 -0600

    Chapter 15
    Finishes chapter 15

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,6 +4,7 @@ import { action } from '@ember/object'
 import { buildValidations } from 'ember-cp-validations'
 import emailFieldValidation from 'rarwe/validations/email-field'
 import passwordFieldValidation from 'rarwe/validations/password-field'
+import extractServerError from 'rarwe/utils/extract-server-error'
 
 const Validations = buildValidations({
   email: emailFieldValidation,
@@ -14,6 +15,7 @@ export default Controller.extend(Validations, {
   init() {
     this._super(...arguments)
     this.set('showErrors', { email: false, password: false })
+    this.set('baseErrors', [])
   },
 
   router: service(),
@@ -21,14 +23,23 @@ export default Controller.extend(Validations, {
 
   signIn: action(async function(e) {
     e.preventDefault()
-    let { email, password } = this
-    await this.session.authenticate('authenticator:credentials', email, password)
-    await this.router.transitionTo('bands')
+    try {
+      let { email, password } = this
+      await this.session.authenticate('authenticator:credentials', email, password)
+      await this.router.transitionTo('bands')
+    } catch (error) {
+      let errorMessage = extractServerError(response.errors)
+      this.baseErrors.pushObject(errorMessage)
+    }
   }),
 
   setShowErrors: action(function(property) {
     let showErrors = { ...this.showErrors }
     showErrors[property] = true
     this.set('showErrors', showErrors)
+  }),
+  
+  resetBaseErrors: action(function() {
+    this.set('baseErrors', [])
   })
 });
