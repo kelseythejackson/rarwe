3175839d685a995d5a208fe2f5cf889e550cbcf2
Kelsey Jackson <kelseythej@gmail.com>
2020-01-16 09:59:23 -0600

    Chapter 17
    Readies app for deployment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,9 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin'
+import ENV from 'rarwe/config/environment'
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
+  host: ENV.apiHost,
   authorize(xhr) {
     let { token } = this.session.data.authenticated
     if(token) {
--- app/authenticators/credentials.js
+++ app/authenticators/credentials.js
@@ -1,9 +1,11 @@
 import Base from 'ember-simple-auth/authenticators/base';
 import fetch from 'fetch'
+import ENV from 'rarwe/config/environment'
 
 export default Base.extend({
   async authenticate(username, password) {
-    let response = await fetch('/token', {
+    let tokenUrl = ENV.apiHost ? `${ENV.apiHost}/token` : '/token'
+    let response = await fetch(tokenUrl, {
       method: 'POST',
       headers: {
         'Accept': 'application/vnd.api+json',
