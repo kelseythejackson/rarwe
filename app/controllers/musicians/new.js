019323eff9725d0b12e0fcf99ea828efc2a7d754
Kelsey Jackson <kelseythej@gmail.com>
2020-01-22 19:59:49 -0600

    Advanced Ember Data
    Editing Musicians is next

--- app/controllers/musicians/new.js
+++ app/controllers/musicians/new.js
@@ -1,38 +1,11 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object'
-import { or } from '@ember/object/computed'
 import { task } from 'ember-concurrency'
 import { inject as service } from '@ember/service'
 
 export default Controller.extend({
   router: service(),
-  store: service(),
 
-  async init() {
-    this._super(...arguments)
-    this.set('showErrors', { name: false })
-    this.set('bands', await this.store.findAll('band'))
-    this.set('selectedBands', [])
-  },
-
-  
-
-  isButtonDisabled: or('model.validations.isInvalid', 'createMusician.isRunning'),
-
-  createMusician: task(function * (e) {
-    e.preventDefault()
-    this.model.set('bands', this.selectedBands)
-    yield this.model.save()
-    yield this.router.transitionTo('musicians.show', this.model.id)
-  }),
-
-  setShowErrors: action(function(property) {
-    let showErrors = {...this.showErrors}
-    showErrors[property] = true
-    this.set('showErrors', showErrors)
-  }),
-
-  updateSelectedBands: action(function(bands) {
-    this.set('selectedBands', bands)
+  gotoMusicianDetails: task(function* () {
+    yield this.router.transitionTo('musician.show', this.model.id)
   })
 });
