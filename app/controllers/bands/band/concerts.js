76284c934e1b75e0dd9d503968aa3be664dbb3f0
Kelsey Jackson <kelseythej@gmail.com>
2020-02-19 18:19:14 -0600

    //WIP Encore - Services
    page 134

--- app/controllers/bands/band/concerts.js
+++ app/controllers/bands/band/concerts.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { readOnly } from '@ember/object/computed';
+import { readOnly, equal } from '@ember/object/computed';
 import { task } from 'ember-concurrency';
 import geolib from 'geolib'
 import { computed } from '@ember/object'
@@ -11,12 +11,19 @@ export default Controller.extend({
   showConcerts: 'all',
   geolocation: service(),
   userLocation: readOnly('geolocation.currentLocation'),
-  concerts: computed('s')
+  showingAll: equal('showConcerts', 'all'),
+  showingNearby: equal('showConcerts', 'nearby'),
+  concerts: computed('showingAll', 'model.[]', function() {
+    if (this.showingAll) {
+      return this.model;
+    }
+    return this.model.slice(0, -1)
+  }),
 
   filterConcerts: task(function * () {
     if (!this.userLocation) {
       yield this.get('geolocation').getLocation();
     }
-    this.set('showConcerts', 'nearby')
+    this.set('showConcerts', this.showingAll ? 'nearby': 'all')
   })
 });
