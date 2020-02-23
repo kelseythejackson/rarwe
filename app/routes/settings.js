a6c81097889acf6f1bc2b4a107861fae2e8793dc
Kelsey Jackson <kelseythej@gmail.com>
2020-02-23 16:52:52 -0600

    Encore - Service
    Adds a user record onto the store and set it as the model for the controller

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1,4 +1,30 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service'
+import fetch from 'fetch'
+import ENV from 'rarwe/config/environment'
 
 export default Route.extend({
+  session: service(),
+
+  async module() {
+    let { token } = this.session.data.authenticated
+    let currentUserURL = '/users/me'
+    if (ENV.apiHost) {
+      currentUserURL = `${ENV.apiHost}/${currentUserURL}`
+    }
+
+    let response = await fetch(currentUserURL, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    let payload = await response.json()
+    let store = this.store
+    let User = store.modelFor('user')
+    let serializer = store.serializerFor('user')
+
+    let jsonApiPayload = serializer.normalizeResponse(store, User, payload, null, 'query')
+
+    return this.store.push(jsonApiPayload)
+  }
 });
