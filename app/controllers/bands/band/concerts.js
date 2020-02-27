f18954a1eaeab20e75df19ba3d28281a8c21199f
Kelsey Jackson <kelseythej@gmail.com>
2020-02-26 23:01:06 -0600

    Encore - Sevices
    page 155 is next

--- app/controllers/bands/band/concerts.js
+++ app/controllers/bands/band/concerts.js
@@ -5,18 +5,22 @@ import { task } from 'ember-concurrency';
 import geolib from 'geolib'
 import { computed, action } from '@ember/object'
 
+const milesToKilometers = 1.61
+
 export default Controller.extend({
   init() {
     this._super(...arguments)
     this.set('nearbyDistances', [200, 500, 1000, 2000])
   },
+  currentUser: service(),
+  useMiles:  readOnly('currentUser.user.prefersImperial'),
   selectedDistance: 500,
   showConcerts: 'all',
   geolocation: service(),
   userLocation: readOnly('geolocation.currentLocation'),
   showingAll: equal('showConcerts', 'all'),
   showingNearby: equal('showConcerts', 'nearby'),
-  concerts: computed('showingAll', 'model.[]', 'userLocation', 'selectedDistance', function() {
+  concerts: computed('showingAll', 'model.[]', 'userLocation', 'maxDistance', function() {
     // If showing all is true, return the whole model
     if (this.showingAll) {
       return this.model;
@@ -27,9 +31,13 @@ export default Controller.extend({
     return this.model.filter((concert) => {
       let { lat, lng } = concert.location
       let distanceToConcert = geolib.getDistance({ lat: userLat, lng: userLng }, {lat, lng})
-      return distanceToConcert < (this.selectedDistance * 1000)
+      return distanceToConcert < this.maxDistance
     })
     
+  }),
+
+  maxDistance: computed('selectedDistance', 'useMiles', function() {
+    return this.selectedDistance * 1000 * (this.useMiles ? milesToKilometers: 1)
   }),
 
   filterConcerts: task(function * () {
